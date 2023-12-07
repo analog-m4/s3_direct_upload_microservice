9b363e5e2fcb21bc69e404a6112c61011c9df5ea
Will Weston <Williamweston81@gmail.com>
2023-12-07 17:14:45 -0600

    Update: local host postman - pass

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  post 'create_presigned_url', to: 'uploaded_files#create_presigned_url'
+  put 'create_presigned_url', to: 'uploaded_files#create_presigned_url'
   post 'upload_complete', to: 'uploaded_files#upload_complete'
 end
 
